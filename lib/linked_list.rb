cc794353cde75df637e2ad854d8b43aef8abf23e
Alice D <alicedilanchian@gmail.com>
2020-12-08 18:38:26 -0500

    time and space complexities

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -7,8 +7,9 @@ class LinkedList
     @head = nil
   end
 
-  # Time complexity - ?
-  # Space complexity - ?
+  # Time complexity - O(1)
+  # Space complexity - O(1) ( You can can only add one node at at time, and each node would
+  # take up the same amount of space no matter what it contains)
   def add_first(data)
     new_node = Node.new(data)
     new_node.next = @head
@@ -16,15 +17,15 @@ class LinkedList
     return @head.data
   end
 
-  # Time complexity - ?
-  # Space complexity - ?
+  # Time complexity - O(1)
+  # Space complexity - O(1)
   def get_first
     return nil if @head.nil?
     return @head.data
   end
 
-  # Time complexity - ?
-  # Space complexity - ?
+  # Time complexity - O(n)
+  # Space complexity - O(1)
   def length
     count = 0
     current = @head
@@ -35,22 +36,21 @@ class LinkedList
     return count
   end
 
-  # Time complexity - ?
-  # Space complexity - ?
+  # Time complexity - O(n)
+  # Space complexity - O(1)
   def add_last(data)
     if @head.nil?
       @head = Node.new(data)
       return @head.data
     else
-      current = self.get_last_node
+      current = self.get_last_node_helper
       current.next = Node.new(data)
       return current.next.data
     end
   end
 
-  # Time complexity - ?
-  # Space complexity - ?
-  def get_last_node
+
+  def get_last_node_helper
     return nil if @head.nil?
     current = @head
     while current.next != nil
@@ -59,13 +59,15 @@ class LinkedList
     return current
   end
 
+  # Time complexity - O(n)
+  # Space complexity - O(1)
   def get_last
     return nil if @head.nil?
-    return get_last_node.data
+    return get_last_node_helper.data
   end
 
-  # Time complexity - ?
-  # Space complexity - ?
+  # Time complexity - O(n)
+  # Space complexity - O(1)
   def get_at_index(index)
     return nil if index > self.length
     current = @head
