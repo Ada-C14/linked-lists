267908db7bd9f435119b3384e8023f7dfd214685
Alice D <alicedilanchian@gmail.com>
2020-12-08 17:14:35 -0500

    initialize, add_first, and get_first passing the tests

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -10,15 +10,17 @@ class LinkedList
   # Time complexity - ?
   # Space complexity - ?
   def add_first(data)
-    new_node = Node.new(value)
+    new_node = Node.new(data)
     new_node.next = @head
     @head = new_node
+    return @head.data
   end
 
   # Time complexity - ?
   # Space complexity - ?
   def get_first
-    return @head.next
+    return nil if @head.nil?
+    return @head.data
   end
 
   # Time complexity - ?
@@ -37,21 +39,26 @@ class LinkedList
   # Time complexity - ?
   # Space complexity - ?
   def add_last(data)
-    new_node = Node.new(value)
-
-    self.each do |node|
-      if node.@next == nil
-      node.@next = data
-      data.@next = nil
-      end
+    current = @head.next
+    while current != nil
+      current = current.next
     end
+    new_node = Node.new(data)
+    current.next = new_node
+
+    # self.each do |node|
+    #   if node.next == nil
+    #   node.next = data
+    #   data.next = nil
+    #   end
+    # end
   end
 
   # Time complexity - ?
   # Space complexity - ?
   def get_last
     self.each do |node|
-      if node.@next == nil
+      if node.next == nil
         return node
       end
     end
