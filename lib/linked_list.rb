c4c4a35226eb97d9748608882facb7b9d806535c
Anna Kim <anna.kim93@gmail.com>
2020-12-07 20:07:51 -0500

    adding to time and space explanations

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -18,7 +18,9 @@ class LinkedList
   end
 
   # Time complexity - O(1)
+  # >> return nil or head data
   # Space complexity - O(1)
+  # >> no new vars
   def get_first
     return @head ? @head.data : nil
   end
@@ -41,7 +43,9 @@ class LinkedList
   end
 
   # Time complexity - O(1)
+  # >> create new node and set as tail node no matter how long list is
   # Space complexity - O(1)
+  # >> one temp node no matter how long list is
   def add_last(data)
     temp = Node.new(data)
     @tail ? @tail.next = temp : @head = temp
@@ -49,7 +53,9 @@ class LinkedList
   end
 
   # Time complexity - O(1)
+  # >> return tail data or nil
   # Space complexity - O(1)
+  # >> creating no new vars
   def get_last
     return @tail ? @tail.data : nil
   end
@@ -57,9 +63,10 @@ class LinkedList
   # Time complexity - O(n)
   # >> have to traverse list to find the node at that index
   # Space complexity - O(1)
+  # >> always tracking current_ind int and current node no matter how long list is
   def get_at_index(index)
-    return @head.data if index.zero?
     return nil unless @head
+    return @head.data if index.zero?
 
     current_ind = 0
     current = @head
