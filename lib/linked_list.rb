839ca56d4b096bf176399c7259fc7399e50e5f21
Ting-Yi Liu <46391352+ichbinorange@users.noreply.github.com>
2020-12-08 21:35:34 -0800

    added methods for add_first & get_first

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -7,16 +7,24 @@ class LinkedList
     @head = nil
   end
 
-  # Time complexity - ?
-  # Space complexity - ?
+  # Time complexity - O(1), it's O(1) to find the head, ie the front of linked list, and add element.
+  # Space complexity - O(1), the extra variables are constant, so it's O(1).
   def add_first(data)
-
+    if @head.nil?
+      @head = Node.new(data)
+    else
+      new_node = Node.new(data)
+      @head.next = new_node
+      @head = new_node
+    end
+    return @head
   end
 
-  # Time complexity - ?
-  # Space complexity - ?
+  # Time complexity - O(1), it's O(1) to find the head, ie the front of linked list, and add element.
+  # Space complexity - O(1), no extra variable is used, so it's O(1).
   def get_first
-
+    return @head.data if @head
+    return nil
   end
 
   # Time complexity - ?
