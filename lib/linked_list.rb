5eaaf8d4e259265952e951b7b8adb03c75aecd31
Ting-Yi Liu <46391352+ichbinorange@users.noreply.github.com>
2020-12-08 23:19:46 -0800

    added method for get_at_index

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -7,7 +7,7 @@ class LinkedList
     @head = nil
   end
 
-  # Time complexity - O(1), it's O(1) to find the head, ie the front of linked list, and add element.
+  # Time complexity - O(1), it's O(1) to find the head, ie the front of linked list, and add an element.
   # Space complexity - O(1), it uses limited variables, ie a constant, so it's O(1).
   def add_first(data)
     if @head
@@ -20,7 +20,7 @@ class LinkedList
     return @head
   end
 
-  # Time complexity - O(1), it's O(1) to find the head, ie the front of linked list, and add element.
+  # Time complexity - O(1), it's O(1) to find the head, ie the front of linked list, and add an element.
   # Space complexity - O(1), no extra variable is used, so it's O(1).
   def get_first
     return @head.data if @head
@@ -32,7 +32,7 @@ class LinkedList
   def length
     count = 0
     current = @head
-    while current != nil
+    while current 
       count += 1
       current = current.next
     end
@@ -45,7 +45,7 @@ class LinkedList
     if @head
       old_next = @head
       current = @head.next
-      while current != nil
+      while current 
         old_next = current
         current = current.next
       end
@@ -63,16 +63,25 @@ class LinkedList
     return nil if @head.nil?
     old_next = @head
     current = @head.next
-    while current != nil
+    while current 
       old_next = current
       current = current.next
     end
     return old_next.data
   end
 
-  # Time complexity - ?
-  # Space complexity - ?
+  # Time complexity - O(n), it needs to go thru the whole linked list to find the index, so the time complexity is O(n).
+  # Space complexity - O(1), it uses limited variables, ie a constant, so it's O(1).
   def get_at_index(index)
-
+    count = 0
+    current = @head
+    while current 
+      if count == index
+        return current.data 
+      end
+      count += 1
+      current = current.next
+    end
+    return nil # for @head.nil & non-existent index
   end
 end
