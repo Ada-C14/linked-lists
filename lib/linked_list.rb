cd90b148169edde1db5d858f6e7b7cbe457c313a
Christabel Sebastian <christabel.escarez@gmail.com>
2020-12-09 18:15:00 -0800

    wrote length

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -7,25 +7,30 @@ class LinkedList
     @head = nil
   end
 
-  # Time complexity - ?
-  # Space complexity - ?
+  # Time complexity - O(1)
+  # Space complexity - O(1)
   def add_first(data)
     @head = Node.new(data, @head)
   end
 
-  # Time complexity - ?
-  # Space complexity - ?
+  # Time complexity - O(1)
+  # Space complexity - O(1)
   def get_first
-    if @head.nil?
-      return nil
-    end
+    return nil if @head.nil?
     return @head.data
   end
 
-  # Time complexity - ?
-  # Space complexity - ?
+  # Time complexity - O(n)
+  # Space complexity - O(1)
   def length
-    return 0
+    return 0 if @head.nil?
+    length = 0
+    current = @head
+    until current.nil?
+      length += 1
+      current = current.next
+    end
+    return length
   end
 
   # Time complexity - ?
