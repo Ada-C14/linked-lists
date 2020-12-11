1d0f338f92d79840384198fb5b6cca2f857688ab
roshni-patel <rpatel01@wesleyan.edu>
2020-12-11 09:49:32 -0600

    Completed linked list methods and time/space complexities

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -7,39 +7,78 @@ class LinkedList
     @head = nil
   end
 
-  # Time complexity - ?
-  # Space complexity - ?
+  # Time complexity - O(1)
+  # Space complexity - O(1)
   def add_first(data)
-
+    new_node = Node.new(data)
+    new_node.next = @head
+    @head = new_node
   end
 
-  # Time complexity - ?
-  # Space complexity - ?
+  # Time complexity - O(1)
+  # Space complexity - O(1)
   def get_first
-
+    return @head ? @head.data : nil
   end
 
-  # Time complexity - ?
-  # Space complexity - ?
+  # Time complexity - O(n)
+  # Space complexity - O(1)
   def length
-    return 0
+    current = @head
+    count = 0
+
+    until current.nil?
+      count += 1
+      current = current.next
+    end
+
+    return count
   end
 
-  # Time complexity - ?
-  # Space complexity - ?
+  # Time complexity - O(n)
+  # Space complexity - O(1)
   def add_last(data)
+    if @head.nil?
+      add_first(data)
+      return
+    end
 
+    current = @head
+    until current.next.nil?
+      current = current.next
+    end
+
+    current.next = Node.new(data)
   end
 
-  # Time complexity - ?
-  # Space complexity - ?
+  # Time complexity - O(n)
+  # Space complexity - O(1)
   def get_last
+    return nil if @head.nil?
 
+    current = @head
+    until current.next.nil?
+      current = current.next
+    end
+
+    return current.data
   end
 
-  # Time complexity - ?
-  # Space complexity - ?
+  # Time complexity - O(n)
+  # Space complexity - O(1)
   def get_at_index(index)
+    return nil if @head.nil? || index >= self.length
 
+    count = 0
+    current = @head
+
+    until current.nil?
+      if count == index
+        return current.data
+      else
+        current = current.next
+        count += 1
+      end
+    end
   end
 end
