23f8f2eaa51737ee5be95acceb1701ef21101e89
Ringo Alcock <ringo.alcock@gmail.com>
2020-12-11 12:31:14 -0800

    define add_first and get_first

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -10,13 +10,18 @@ class LinkedList
   # Time complexity - ?
   # Space complexity - ?
   def add_first(data)
-
+    if @head.nil?
+      @head = Node.new(data)
+    else
+      new_node = Node.new(data, @head)
+      @head = new_node
+    end
   end
 
   # Time complexity - ?
   # Space complexity - ?
   def get_first
-
+    return (@head.nil? ? @head : @head.data)
   end
 
   # Time complexity - ?
