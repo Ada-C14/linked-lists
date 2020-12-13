e00e10b4438df7cb0a453a092c210b65bc53de20
Kayla Johnson <kayla.elizabeth89@gmail.com>
2020-12-12 20:57:39 -0800

    add_first and get_first passing; working on length method

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -7,22 +7,32 @@ class LinkedList
     @head = nil
   end
 
-  # Time complexity - ?
-  # Space complexity - ?
+  # Time complexity - O(1)--only adding one value (reassignment from nil)
+  # Space complexity - O(1)--simple reassignment, not creating anything new
   def add_first(data)
-
+    @head = Node.new(data) #@head is now being assigned to new value (data)
+    return @head
   end
 
-  # Time complexity - ?
-  # Space complexity - ?
+  # Time complexity - O(1)--don't have to traverse anything. Simply returning first value.
+  # Space complexity - O(1)--space is not added
   def get_first
-
+    return nil if @head.nil?
+    return @head.data #(returning value (.data) instead of Nodes object)
   end
 
   # Time complexity - ?
   # Space complexity - ?
   def length
-    return 0
+    return 0 if @head.nil?
+    return 1 if @head.next.nil?
+    counter = 0
+    current = @head
+    until current.nil?
+      current = current.next
+      counter += 1
+      return counter
+    end
   end
 
   # Time complexity - ?
