17fb66c5682bdae85e65f56801cda8e7f925b07e
Ren Carothers <rencarothers@gmail.com>
2020-12-13 13:09:41 -0800

    adds functions and t/s complexity

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -7,39 +7,78 @@ class LinkedList
     @head = nil
   end
 
-  # Time complexity - ?
-  # Space complexity - ?
+  # Time complexity - O(1)
+  # Space complexity - O(1)
   def add_first(data)
-
+    @head = Node.new(data, @head)
   end
 
-  # Time complexity - ?
-  # Space complexity - ?
+  # Time complexity - O(1)
+  # Space complexity - O(1)
   def get_first
-
+    return nil if @head.nil?
+    return @head.data
   end
 
-  # Time complexity - ?
-  # Space complexity - ?
+  # Time complexity - O(n), where n is the length of linked list
+  # Space complexity - O(1)
   def length
-    return 0
+    return 0 if @head.nil?
+
+    count = 0
+    current = @head
+
+    while current
+      count += 1
+      current = current.next
+    end
+
+    return count
   end
 
-  # Time complexity - ?
-  # Space complexity - ?
+  # Time complexity - O(n), where n is the length of linked list
+  # Space complexity - O(1)
   def add_last(data)
+    if @head.nil?
+      @head = Node.new(data)
+    else
+      last_node = Node.new(data)
 
+      current = @head
+      until current.next.nil?
+        current = current.next
+      end
+      current.next = last_node
+    end
   end
 
-  # Time complexity - ?
-  # Space complexity - ?
+  # Time complexity - O(n), where n is the length of linked list
+  # Space complexity - O(1)
   def get_last
+    return nil if @head.nil?
 
+    current = @head
+    until current.next.nil?
+      current = current.next
+    end
+
+    return current.data
   end
 
-  # Time complexity - ?
-  # Space complexity - ?
+  # Time complexity - O(n), where n is value of index
+  # Space complexity - O(1)
   def get_at_index(index)
+    return nil if @head.nil?
 
+    current = @head
+    count = 0
+
+    until count == index
+      return nil if !current
+      current = current.next
+      count += 1
+    end
+
+    return current.data
   end
 end
