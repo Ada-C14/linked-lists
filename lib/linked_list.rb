fee2bbff439a8d76c46f4711d72223616c5c2913
Yingjing Lin <yjlin789@gmail.com>
2020-12-13 16:44:56 -0800

    complete methods

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -7,39 +7,61 @@ class LinkedList
     @head = nil
   end
 
-  # Time complexity - ?
-  # Space complexity - ?
+  # Time complexity O(1)
+  # Space complexity O(1)
   def add_first(data)
-
+    a = Node.new(data)
+    a.next = @head
+    @head = a
   end
 
-  # Time complexity - ?
-  # Space complexity - ?
+  # Time complexity - O(1)
+  # Space complexity - O(0)????
   def get_first
-
+    return @head.data
   end
 
-  # Time complexity - ?
-  # Space complexity - ?
+  # Time complexity - O(n)
+  # Space complexity - O(1)
   def length
-    return 0
+    length = 0
+    current = @head
+    until current.nil?
+      length += 1
+      current = current.next
+    end
+    return length
   end
 
-  # Time complexity - ?
-  # Space complexity - ?
+  # Time complexity - O(n)
+  # Space complexity - O(1)
   def add_last(data)
-
+    current = @head
+    until current.nil?
+      current = current.next
+    end
+    current = Node.new(data)
   end
 
-  # Time complexity - ?
-  # Space complexity - ?
+  # Time complexity - O(n)
+  # Space complexity - O(1)
   def get_last
-
+    current = @head
+    until current.next.nil?
+      current = current.next
+    end
+    return current.data
   end
 
-  # Time complexity - ?
-  # Space complexity - ?
+  # Time complexity - O(index)
+  # Space complexity - O(1)
   def get_at_index(index)
-
+    current = @head
+    i = 0
+    while i < index
+      current = current.next
+      i += 1
+    end
+    return current.data
   end
 end
