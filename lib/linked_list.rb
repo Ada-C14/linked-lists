7e05f603b3793cc6d3169933f42986b6aeed910a
Noor B.A <noor.bahr.aluloom@gmail.com>
2020-12-14 13:55:43 -0800

    finalized linked list methods

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -7,39 +7,77 @@ class LinkedList
     @head = nil
   end
 
-  # Time complexity - ?
-  # Space complexity - ?
+  # Time complexity - O(1)
+  # Space complexity - O(1)
   def add_first(data)
-
+    if @head.nil?
+      @head = Node.new(data)
+    else
+      temp = Node.new(data, @head) # making a new head
+      @head = temp
+    end
   end
 
-  # Time complexity - ?
-  # Space complexity - ?
+  # Time complexity - O(1)
+  # Space complexity - O(1)
   def get_first
-
+    return nil if @head.nil?
+    return @head.data
   end
 
-  # Time complexity - ?
-  # Space complexity - ?
+  # Time complexity - O(n)
+  # Space complexity - O(1)
   def length
-    return 0
+    return 0 if @head.nil?
+    # return 1 if @head.next = nil
+
+    count = 1
+    current = @head
+    until current.next == nil
+      current = current.next
+      count += 1
+    end
+    return count
   end
 
-  # Time complexity - ?
-  # Space complexity - ?
+  # Time complexity - O(n)
+  # Space complexity - O(1)
   def add_last(data)
-
+    if @head.nil?
+      @head = Node.new(data)
+      return
+    end
+    last = @head
+    until last.next.nil?
+      last = last.next
+    end
+    last.next = Node.new(data)
   end
 
-  # Time complexity - ?
-  # Space complexity - ?
+  # Time complexity - O(n)
+  # Space complexity - O(1)
   def get_last
-
+    return nil if @head.nil?
+    last = @head
+    until last.next == nil
+      last = last.next
+    end
+    return last.data
   end
 
-  # Time complexity - ?
-  # Space complexity - ?
+  # Time complexity - O(n)
+  # Space complexity - O(1)
   def get_at_index(index)
 
+    current = @head
+    index.times do
+      return nil if current.nil?
+
+      current = current.next
+    end
+
+    return nil if current.nil?
+
+    current.data
   end
 end
