74811e3909ba5bd74fd617a85177fb7695f55ad3
Jasmine <jasylop@gmail.com>
2020-12-13 21:00:53 -0800

    implemented methods for linked list

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -5,41 +5,76 @@ class LinkedList
 
   def initialize
     @head = nil
-  end
+   end
 
   # Time complexity - ?
   # Space complexity - ?
   def add_first(data)
-
+    if @head
+      @head = Node.new(data, @head)
+    else
+      @head = Node.new(data)
+    end
   end
 
-  # Time complexity - ?
-  # Space complexity - ?
+  # Time complexity - O(1)
+  # Space complexity - O(1)
   def get_first
-
+    return @head.nil? ? @head : @head.data
   end
 
   # Time complexity - ?
   # Space complexity - ?
   def length
-    return 0
+    return 0 if @head.nil?
+
+    count = 1
+    current = @head
+    while current.next
+      count += 1
+      current = current.next
+    end
+    return count
   end
 
   # Time complexity - ?
   # Space complexity - ?
   def add_last(data)
-
+    if @head.nil?
+      @head = Node.new(data)
+    else
+      tail = Node.new(data)
+      current = @head
+      while current.next
+        current = current.next
+      end
+      current.next = tail
+    end
   end
 
-  # Time complexity - ?
-  # Space complexity - ?
+  # Time complexity - O(n)
+  # Space complexity - O(1)
   def get_last
-
+    return @head if @head.nil?
+    current = @head
+    while current.next
+      current = current.next
+    end
+    return current.data
   end
 
-  # Time complexity - ?
-  # Space complexity - ?
+  # Time complexity - O(n)
+  # Space complexity - O(1)
   def get_at_index(index)
 
+    return nil if index > self.length - 1
+
+    count = 0
+    current = @head
+    while count < index
+      count += 1
+      current = current.next
+    end
+    return current.data
   end
 end
