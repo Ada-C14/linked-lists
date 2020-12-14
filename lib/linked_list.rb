ee30c77b9f8dac00824f744b780a5d6b3231a68b
Stacy <lundquist.stacy@gmail.com>
2020-12-14 08:20:36 -0800

    one test failing

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -10,36 +10,62 @@ class LinkedList
   # Time complexity - ?
   # Space complexity - ?
   def add_first(data)
-
+    new_node = Node.new(data)
+    new_node.next = @head
+    @head = new_node
   end
 
   # Time complexity - ?
   # Space complexity - ?
   def get_first
-
+    @head.nil? ? nil : @head.data
   end
 
   # Time complexity - ?
   # Space complexity - ?
   def length
-    return 0
+    count = 0
+    current = @head
+    until current.nil?
+      count += 1
+      current = current.next
+    end
+    return count
   end
 
   # Time complexity - ?
   # Space complexity - ?
   def add_last(data)
+    add_first(data) if @head.nil?
+    current = @head
+    until current.next.nil?
+      current = current.next
+    end
+    current.next = Node.new(data)
 
   end
 
   # Time complexity - ?
   # Space complexity - ?
   def get_last
-
+    return nil if @head.nil?
+    current = @head
+    until current.next.nil?
+      current = current.next
+    end
+    return current.data
   end
 
   # Time complexity - ?
   # Space complexity - ?
   def get_at_index(index)
-
+    return nil if @head.nil?
+    current = @head
+    index.times do
+      return nil if current.nil?
+      current = current.next
+    end
+    return current.data
   end
+
 end
