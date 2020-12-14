24995d940118908c8e9e99d3ab7a66a2476eca4b
Sophie Messing <sophie.e.messing@gmail.com>
2020-12-13 18:20:10 -0800

    wrote all methods

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -10,36 +10,61 @@ class LinkedList
   # Time complexity - ?
   # Space complexity - ?
   def add_first(data)
-
+    @head = Node.new(data, @head)
   end
 
   # Time complexity - ?
   # Space complexity - ?
   def get_first
-
+    return @head ? @head.data : nil
   end
 
   # Time complexity - ?
   # Space complexity - ?
   def length
-    return 0
+    count = 0
+    current = @head
+    until current.nil?
+      count += 1
+      current = current.next
+    end
+    return count
   end
 
   # Time complexity - ?
   # Space complexity - ?
   def add_last(data)
-
+    last = Node.new(data)
+    if @head.nil?
+      @head = last
+    else
+      current = @head
+      until current.next.nil?
+        current = current.next
+      end
+      current.next = last
+    end
   end
 
   # Time complexity - ?
   # Space complexity - ?
   def get_last
-
+    return nil if head.nil?
+    current = @head
+    until current.next.nil?
+      current = current.next
+    end
+    return current.data
   end
 
   # Time complexity - ?
   # Space complexity - ?
   def get_at_index(index)
-
+    current = @head
+    index.times do
+      return nil if current.nil?
+      current = current.next
+    end
+    return current.data
   end
 end
