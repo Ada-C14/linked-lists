5ae3fd0297e3e57d106ba8454da79dee6bc39b9b
Iris Lux <iris.lux0@gmail.com>
2020-12-14 20:52:05 -0800

    completed

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -10,36 +10,44 @@ class LinkedList
   # Time complexity - ?
   # Space complexity - ?
   def add_first(data)
-
+    @head = Node.new(data, @head)
   end
 
   # Time complexity - ?
   # Space complexity - ?
   def get_first
-
+    return @head if @head.nil?
+    return @head.data
   end
 
   # Time complexity - ?
   # Space complexity - ?
-  def length
-    return 0
+  def length(node = @head, len = 0)
+    return len if node.nil?
+    return length(node.next, len + 1)
   end
 
   # Time complexity - ?
   # Space complexity - ?
-  def add_last(data)
-
+  def add_last(data, node = @head)
+    add_first(data) and return if node.nil?
+    node.next = Node.new(data, nil) and return if node.next.nil?
+    add_last(data, node.next)
   end
 
   # Time complexity - ?
   # Space complexity - ?
-  def get_last
-
+  def get_last(node = @head)
+    return node if node.nil?
+    return node.data if node.next.nil?
+    return get_last(node.next)
   end
 
   # Time complexity - ?
   # Space complexity - ?
-  def get_at_index(index)
-
+  def get_at_index(index, node = @head)
+    return nil if index >= self.length
+    return node.data if index == 0 || node.next.nil?
+    return get_at_index(index - 1, node.next)
   end
 end
